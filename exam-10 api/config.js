7eb2a0f560ee4f2377e346f881ff897e12761fb4
maksimus <gorlovmaks23@gmail.com>
2020-10-24 19:47:45 +0600

    Добавлен файл config.js

---
+++ exam-10 api/config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const rootPath = __dirname;
+
+module.exports = {
+    rootPath,
+    uploadPath: path.join(rootPath, "public/uploads"),
+    db: {
+        host: 'localhost',
+        user: 'root',
+        password: '123',
+        database: 'exam10'
+    }
+};
